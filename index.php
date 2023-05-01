12e7a76e66b5043a15040adf3dfec00843c35b07
SavyonMalko <124379798+SavyonMalko@users.noreply.github.com>
2023-05-01 21:10:49 +0000

    done all now

--- index.php
+++ index.php
@@ -28,6 +28,7 @@
     <form method="post" action="">
         <label for="timbits">Choose your pack of Timbits:</label>
         <select id="timbits" name="timbits">
+            <option value="" selected disabled>Please select a size</option>
             <option value="3.19">10 pack - $3.19</option>
             <option value="4.99">20 pack - $4.99</option>
             <option value="9.99">50 pack - $9.99</option>
@@ -51,6 +52,7 @@
 
         <label for="coffee">Choose your coffee size:</label>
         <select id="coffee" name="coffee">
+            <option value="" selected disabled>Please select a size</option>
             <option value="1.59">Small - $1.59</option>
             <option value="1.79">Medium - $1.79</option>
             <option value="1.99">Large - $1.99</option>
@@ -62,97 +64,106 @@
 
           <?php
 
-//get form inputs from user
 if (isset($_POST['submit'])) {
-    $pack = $_POST['timbits'];
+    $pack = isset($_POST['timbits']) ? $_POST['timbits'] : '';
     $toppings = isset($_POST['toppings']) ? $_POST['toppings'] : array();
     $secondToppings = isset($_POST['secondToppings']) ? $_POST['secondToppings'] : array();
-    $coffee = $_POST['coffee'];
+    $coffee = isset($_POST['coffee']) ? $_POST['coffee'] : '';
 
-    $pack_cost = 0;
-    $topping_cost = 0;
-    $secondTopping_cost = 0;
+    // Check if any selections are made
+    if (empty($pack) && empty($toppings) && empty($secondToppings) && empty($coffee)) {
+      
+        // Display error message
+        echo "<p>Please select what you would like to order.</p>";
+    } else {
+        $pack_cost = 0;
+        $topping_cost = 0;
+        $secondTopping_cost = 0;
 
-    // Determine the cost of the selected pack of Timbits
-    switch ($pack) {
-        case '3.19':
-            $pack_cost = 3.19;
-            break;
-        case '4.99':
-            $pack_cost = 4.99;
-            break;
-        case '9.99':
-            $pack_cost = 9.99;
-            break;
-        default:
-            break;
-    }
-
-    // Calculate the cost of selected toppings
-    foreach ($toppings as $topping) {
-        switch ($topping) {
-            case '0.02':
-                $topping_cost += 0.02;
+        // Determine the cost of the selected pack of Timbits
+        switch ($pack) {
+            case '3.19':
+                $pack_cost = 3.19;
+                break;
+            case '4.99':
+                $pack_cost = 4.99;
+                break;
+            case '9.99':
+                $pack_cost = 9.99;
                 break;
             default:
                 break;
         }
-    }
 
-    // Calculate the cost of selected second toppings
-    foreach ($secondToppings as $secondTopping) {
-        switch ($secondTopping) {
-            case '0.02':
-                $secondTopping_cost += 0.02;
+        // Calculate the cost of selected toppings
+        foreach ($toppings as $topping) {
+            switch ($topping) {
+                case '0.02':
+                    $topping_cost += 0.02;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        // Calculate the cost of selected second toppings
+        foreach ($secondToppings as $secondTopping) {
+            switch ($secondTopping) {
+                case '0.02':
+                    $secondTopping_cost += 0.02;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        // Determine the cost of the selected coffee size
+        switch ($coffee) {
+            case '1.59':
+                $coffee_cost = 1.59;
+                break;
+            case '1.79':
+                $coffee_cost = 1.79;
+                break;
+            case '1.99':
+                $coffee_cost = 1.99;
+                break;
+            case '0':
+                $coffee_cost = 0;
                 break;
             default:
                 break;
         }
-    }
 
-    // Determine the cost of the selected coffee size
-    switch ($coffee) {
-        case '1.59':
-            $coffee_cost = 1.59;
-            break;
-        case '1.79':
-            $coffee_cost = 1.79;
-            break;
-        case '1.99':
-            $coffee_cost = 1.99;
-            break;
-       case '0':
-            $coffee_cost = 0;
-            break;
-        default:
-            break;
-    }
-
-    // Calculate the subtotal and total of the order
-    $subtotal = number_format(($pack_cost * 1) + ($topping_cost * count($toppings)) + ($secondTopping_cost * count($secondToppings)) + ($coffee_cost * 1), 2);
-    $total = number_format($subtotal * 1.13, 2);
-    ?>
-    <h2>Your Order Summary</h2>
-    <ul>
-        <li>Pack of Timbits for $<?php echo $pack_cost; ?></li>
-        <?php
-        if (!empty($toppings)) {
-            echo "<li>Toppings: $" . implode(", ", $toppings) . ", at $" . $topping_cost . "</li>";
-        }
-        if (!empty($secondToppings)) {
-            echo "<li>Extra Topping: $" . implode(", ", $secondToppings) . ", at $" . $secondTopping_cost . "</li>";
-        }
+        // Calculate the subtotal and total of the order
+        $subtotal = number_format(($pack_cost * 1) + ($topping_cost * count($toppings)) + ($secondTopping_cost * count($secondToppings)) + ($coffee_cost * 1), 2);
+        $total = number_format($subtotal * 1.13, 2);
         ?>
-        <li>Coffee: $<?php echo $coffee_cost; ?>, at $<?php echo $coffee_cost; ?> per cup</li>
-        <li>Subtotal: $<?php echo $subtotal; ?></li>
-        <li>Total (including 13% HST): $<?php echo $total; ?></li>
-    </ul>
-
+        <h2>Your Order Summary</h2>
+        <ul>
+            <li>Pack of Timbits for $<?php echo $pack_cost; ?></li>
+            <?php
+            if (!empty($toppings)) {
+                echo "<li>Toppings: $" . implode(", ", $toppings) . ", at $" . $topping_cost . "</li>";
+            }
+            if (!empty($secondToppings)) {
+                echo "<li>Extra Topping: $" . implode(", ", $secondToppings) . ", at $" . $secondTopping_cost . "</li>";
+            }
+                  ?>
+            <li>Coffee: $<?php echo $coffee_cost; ?>, at $<?php echo $coffee_cost; ?> per cup</li>
+            <li>Subtotal: $<?php echo $subtotal; ?></li>
+            <li>Total (including 13% HST): $<?php echo $total; ?></li>
+        </ul>
     <?php
+    }
+} elseif (isset($_POST['submit'])) {
+  
+    // Display message if the form has been submitted without any selections
+    echo "<p>Please select what you would like to order.</p>";
 }
-
-// GIF on this page
+  //GIF on this page
 echo '<img src="./images/timbits.gif" alt="timbits" height="200" width="200">';
 
-// Image on this page
+//Image on this page
 echo '<img src="./images/coffee.webp" alt="coffee" height="200" width="200">';
+?>
